df8f4fed671b66e8bad9948bb7775f9e69541d9e
ia <isaac.ardis@gmail.com>
2017-12-13 15:50:15 -0600

    :floppy_disk:

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
 var Web3 = require('web3');
 var web3 = new Web3();
 
-web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
-
-// console.log(web3);
-
-// var bn = web3.eth.blockNumber;
-// console.log(bn);
-
-var v = web3.version.api;
-console.log(v);
+web3.setProvider(new web3.providers.WebsocketProvider('ws://localhost:8546'));
 
 var vv = web3.version;
 console.log(vv);
 
-web3.version.getNetwork(function(error, result){
-    console.lot(error, result);
+// Works on RPC, not WS.
+//var b = web3.eth.getBlock('latest').then(function(res, err) {
+//    console.log(res, err);
+//});
+// Also not working -- 'connection not open on send()'
+web3.eth.getBlock('latest', function (res, err){
+    console.log(res, err);
 });
 
-//var coinbase = web3.eth.coinbase;
-//console.log(coinbase);
-//
-//var balance = web3.eth.getBalance(coinbase);
-//console.log(balance.toString(10));
+console.log(web3.eth.defaultBlock);
+
